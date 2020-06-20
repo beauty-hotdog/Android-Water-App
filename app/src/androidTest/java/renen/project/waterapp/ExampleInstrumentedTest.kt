b633d7d4210d7607c829d70aa10407d080e35f82
YA ANUTA <anakopa7@mail.ru>
2020-06-20 14:53:27 +0300

    ui unit tests

--- app/src/androidTest/java/renen/project/waterapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/renen/project/waterapp/ExampleInstrumentedTest.kt
@@ -1,12 +1,20 @@
 package renen.project.waterapp
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +22,37 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityRule: ActivityTestRule<MainActivity>
+            = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("renen.project.waterapp", appContext.packageName)
+    }
+
+    @Test
+    fun graph_test(){
+        onView(withId(R.id.slimChart))            // withId(R.id.my_view) is a ViewMatcher
+            .perform(click())               // click() is a ViewAction
+            .check(matches(isDisplayed()))
+        Thread.sleep(1000)
+    }
+    @Test
+    fun history_test(){
+        onView(withId(R.id.btnHistory))            // withId(R.id.my_view) is a ViewMatcher
+            .perform(click())               // click() is a ViewAction
+        Thread.sleep(1000)
+    }
+    @Test
+    fun settings_test(){
+        onView(withId(R.id.btnSettings))            // withId(R.id.my_view) is a ViewMatcher
+            .perform(click())               // click() is a ViewAction
+        Thread.sleep(1000)
     }
 }
--- app/src/test/java/renen/project/waterapp/ExampleUnitTest.kt
+++ app/src/test/java/renen/project/waterapp/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package renen.project.waterapp
 import org.junit.Test
 
 import org.junit.Assert.*
+import renen.project.waterapp.util.isLastElement
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +14,15 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+    @Test
+    fun lastElement_test(){
+        val a = "a"
+        val b = "b"
+        val c = "c"
+        val list = listOf<String>(a, b, c)
+        assertEquals(false, isLastElement(position = list.indexOf(a), listSize = list.size))
+        assertEquals(false, isLastElement(position = list.indexOf(b), listSize = list.size))
+        assertEquals(true, isLastElement(position = list.indexOf(c), listSize = list.size))
     }
 }
