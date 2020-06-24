8473f07d578a98aa7f22466475f5adaa49ebce49
YA ANUTA <anakopa7@mail.ru>
2020-06-24 13:45:26 +0300

    Googl Play bottom

--- app/src/main/java/renen/project/waterapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/renen/project/waterapp/ui/settings/SettingsFragment.kt
@@ -1,14 +1,18 @@
 package renen.project.waterapp.ui.settings
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_settings.*
 import renen.project.waterapp.R
+
+
 
 class SettingsFragment : Fragment() {
 
@@ -28,6 +32,27 @@ class SettingsFragment : Fragment() {
 
         clTheme.setOnClickListener {
             rbTheme.isChecked = !rbTheme.isChecked
+        }
+
+        clGooglePlay.setOnClickListener {
+            val appPackageName: String =
+                requireContext().getPackageName() // getPackageName() from Context or Activity object
+
+            try {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("market://details?id=$appPackageName")
+                    )
+                )
+            } catch (anfe: ActivityNotFoundException) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://play.google.com/store/apps/details?id=$appPackageName")
+                    )
+                )
+            }
         }
     }
 }
